620765e6bdae7bc34ae3cedade38a784e7c3246e
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-06-11 04:27:27 -0400

    enhance styling

--- run.js
+++ run.js
@@ -4,21 +4,41 @@ var request = require('request');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const _ = require('underscore');
+const chalk = require('chalk');
 
-const SCHOOL_NAME = "UOTTAWA";
 
-console.log();
-console.log('===================================');
-console.log(SCHOOL_NAME, 'CALENDAR GENERATOR');
-console.log('===================================');
-console.log();
+printTitle();
 
 uOttawascheduleCrawler();
 
 
+/* FUNCTIONS */
+function printTitle() {
+	const SCHOOL_NAME = "POLY"; // change school name
+	const TITLE = SCHOOL_NAME + ' CALENDAR GENERATOR';
+	const BORDER = generateLine(TITLE.length, '*');
+	const PADDING = generateLine(TITLE.length, ' ');
+
+	console.log();
+	console.log(chalk.cyan(BORDER));
+	console.log(chalk.cyan(PADDING));
+	console.log(chalk.cyan('*'), chalk.white.bold(SCHOOL_NAME, 'CALENDAR GENERATOR'), chalk.cyan('*'));
+	console.log(chalk.cyan(PADDING));
+	console.log(chalk.cyan(BORDER));
+	console.log();
+
+	function generateLine(len, char) {
+		var line = '*';
+
+		for (var i = 0; i < len + 2; i++) // + 4 for padding
+			line += char;
+
+		return line + '*';
+	}
+}
 
 function uOttawascheduleCrawler() {
-	console.log("== GRABBING THE FORM ID");
+	console.log("GRABBING THE FORM ID");
 
 	request({
 		url: 'https://uozone2.uottawa.ca/user/login',
@@ -31,7 +51,7 @@ function uOttawascheduleCrawler() {
 		if (!formId)
 			throw new Error('Form id not found');
 
-		console.log("== FORM ID GOTTEN\n");
+		console.log("FORM ID GOTTEN\n");
 
 		askUozoneCredentials(function(creds) {
 		 	request({
@@ -118,11 +138,11 @@ function uOttawascheduleCrawler() {
 		var $ = cheerio.load(body);
 
 		if ($('body').hasClass('logged-in')) {
-			console.log('Good credentials');
+			console.log(chalk.green('\nGood credentials\n'));
 			callback();
 		}
 		else
-			console.log('Bad uoZone credentials');
+			console.log(chalk.red('Bad uoZone credentials'));
 	}
 
 	function getSemesters($) {
@@ -143,7 +163,7 @@ function uOttawascheduleCrawler() {
 		var curId = chosenSemesters.semesters[curKey];
 		delete chosenSemesters.semesters[curKey];
 
-		console.log('Grabbing schedule of', curKey, curId);
+		console.log('Grabbing schedule of', chalk.magenta(curKey));
 
 		// TODO: CHANGE TIMEOUT WITH ACTUAL SCHEDULE GRABBING/PARSING
 		setTimeout(function() {
